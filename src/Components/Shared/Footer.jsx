b868627a9b3a955e70fa4d46929e57cc45af3e4b
FarhanFardid <farhanfardid009@gmail.com>
2023-05-18 19:41:48 +0600

    icons added from react icons

--- src/Components/Shared/Footer.jsx
+++ src/Components/Shared/Footer.jsx
@@ -1,4 +1,5 @@
 import logo from '../../assets/Logo/kid_logo-1.png'
+import { FaFacebookSquare, FaInstagramSquare, FaTwitterSquare } from "react-icons/fa";
 
 const Footer = () => {
     
@@ -6,36 +7,36 @@ const Footer = () => {
             <footer className="footer bg-slate-900 md:p-12 p-5 text-white  ">
               <div className="footer__logo">
                 <img src={logo} className='w-12 h-12 rounded-full mx-auto' alt="Kids Heaven Logo" />
-                <h2 className='text-lg font-bold'>Kids Heaven</h2>
+                <h2 className='text-lg font-bold text-purple-300'>Kids Heaven</h2>
               </div>
               <div className="footer__contact">
                 <h3 className='font-bold text-red-600 text-base'>Contact Information</h3>
-                <p className='text-xs'>Phone: 123-456-7890</p>
-                <p className='text-xs'>Email: info@kidsheaven.com</p>
+                <p className='text-xs font-medium'>Phone: 123-456-7890</p>
+                <p className='text-xs font-medium'>Email: info@kidsheaven.com</p>
               </div>
               <div className="footer__social">
                 <h3 className="font-bold text-red-600 text-base">Follow Us</h3>
-                <ul>
-                  <li>
-                    <a href="https://www.facebook.com/kidsheaven">
-                      <i className="fab fa-facebook"></i>
+                <ul className='flex '>
+                  <li className='pr-2'>
+                    <a href="#">
+                      <FaFacebookSquare className='w-4 h-4' />
                     </a>
                   </li>
-                  <li>
-                    <a href="https://www.instagram.com/kidsheaven">
-                      <i className="fab fa-instagram"></i>
+                  <li className='pr-2'>
+                    <a href="#">
+                     <FaInstagramSquare className='w-4 h-4'/>
                     </a>
                   </li>
-                  <li>
-                    <a href="https://www.twitter.com/kidsheaven">
-                      <i className="fab fa-twitter"></i>
+                  <li className='pr-2'>
+                    <a href="#" >
+                 <FaTwitterSquare className='w-4 h-4'/>
                     </a>
                   </li>
                 </ul>
               </div>
               <div className="footer__address">
                 <h3 className="font-bold text-red-600 text-base">Address</h3>
-                <p className='text-xs'>123 Main Street, London, UK</p>
+                <p className='text-xs font-medium'>123 Main Street, London, UK</p>
               </div>
               <div className="md:mt-5">
                 <p className="font-bold text-red-600 text-base ">&copy; {new Date().getFullYear()} Kids Heaven. All rights reserved.</p>
