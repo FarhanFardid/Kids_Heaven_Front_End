da61a49c8114f33171bec39a78b02a406a915c99
FarhanFardid <farhanfardid009@gmail.com>
2023-05-19 19:46:37 +0600

    slider completed

--- src/Components/Home/Slider.jsx
+++ src/Components/Home/Slider.jsx
@@ -17,7 +17,7 @@ const Slider = () => {
             </div>
             </div>
             <div className="absolute flex  right-14 bottom-10">
-            <a href="#slide4" className="btn btn-circle mr-4  bg-red-600">❮</a> 
+            <a href="#slide3" className="btn btn-circle mr-4  bg-red-600">❮</a> 
             <a href="#slide2" className="btn btn-circle bg-red-600">❯</a>
           </div>
         </div> 
@@ -25,32 +25,31 @@ const Slider = () => {
           <img src={banner3} className="w-full h-full rounded-xl" />
           <div className="absolute h-full flex items-center  left-0 top-0  rounded-xl bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
             <div className='text-white space-y-7 pl-12 w-2/3 h-full pt-20  '>
-                <h2 className='text-5xl text-white p-2 font-bold'>Affordable Price For Car Servicing</h2>
-                <p className='p-2'>Take care of your car in the garage, and the car will take care of you on the road</p>
-                <div>   <button className="btn bg-orange-600 mx-3">Discover More</button>
-                <button className="btn btn-outline text-white  border-4 border-white">Latest Projects</button></div>
+            <h2 className='text-5xl text-red-600 p-2 font-bold'>Bringing the Animal Kingdom to Your Doorstep...</h2>
+                <p className='p-2 text-xl'>Let The Imagination Run Wild with Our Amazing Animal Toy Collection. Discover a Heaven of Animals.</p>
+                <div>   <button className="btn bg-red-700 mx-3">Explore Kids Heaven</button>
+               </div>
              
             </div>
             </div>
-            <div className="absolute flex   right-11 bottom-1">
-            <a href="#slide1" className="btn btn-circle mr-4 bg-orange-500">❮</a> 
-            <a href="#slide3" className="btn btn-circle bg-orange-500">❯</a>
+            <div className="absolute flex  right-14 bottom-10">
+            <a href="#slide1" className="btn btn-circle mr-4  bg-red-600">❮</a> 
+            <a href="#slide3" className="btn btn-circle bg-red-600">❯</a>
           </div>
         </div> 
         <div id="slide3" className="carousel-item relative w-full h-full  ">
           <img src={banner1} className="w-full h-full rounded-xl" />
           <div className="absolute h-full flex items-center  left-0 top-0  rounded-xl bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
             <div className='text-white space-y-7 pl-12 w-2/3 h-full pt-20  '>
-                <h2 className='text-5xl text-white p-2 font-bold'>Affordable Price For Car Servicing</h2>
-                <p className='p-2'>Take care of your car in the garage, and the car will take care of you on the road</p>
-                <div>   <button className="btn bg-orange-600 mx-3">Discover More</button>
-                <button className="btn btn-outline text-white  border-4 border-white">Latest Projects</button></div>
-             
+            <h2 className='text-5xl text-red-600 p-2 font-bold'>Bringing the Animal Kingdom to Your Doorstep...</h2>
+                <p className='p-2 text-xl'>Let The Imagination Run Wild with Our Amazing Animal Toy Collection. Discover a Heaven of Animals.</p>
+                <div>   <button className="btn bg-red-700 mx-3">Explore Kids Heaven</button>
+               </div>
             </div>
             </div>
-            <div className="absolute flex   right-11 bottom-1">
-            <a href="#slide2" className="btn btn-circle mr-4 bg-orange-500">❮</a> 
-            <a href="#slide1" className="btn btn-circle bg-orange-500">❯</a>
+            <div className="absolute flex  right-14 bottom-10">
+            <a href="#slide2" className="btn btn-circle mr-4  bg-red-600">❮</a> 
+            <a href="#slide1" className="btn btn-circle bg-red-600">❯</a>
           </div>
         </div> 
    
