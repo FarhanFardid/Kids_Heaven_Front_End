543a763d35e27a35df850da25cbd855643a4b363
FarhanFardid <farhanfardid009@gmail.com>
2023-05-20 18:34:20 +0600

    My toys delete button functional

---
+++ src/Components/Toys/MyToyRows.jsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import { FaArrowCircleUp } from "react-icons/fa";
+
+const MyToyRows = ({toy,handleDelete}) => {
+    const{_id,sellerName,toyName,image,price,quantity,subCategory} =toy;
+    return (
+        <tr className="p-2">
+  <th></th>
+        <td>
+          <div className="flex items-center space-x-3">
+            <div className="avatar">
+              <div className="mask mask-squircle w-20 h-20">
+                <img src={image} alt="toy image" />
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">{toyName}</div>
+              
+            </div>
+          </div>
+        
+        </td>
+        <td className="font-bold">{sellerName}</td>
+        <td className="font-bold">
+          {subCategory}
+        </td>
+        <td className="font-bold ">{price}</td>
+        <td className="font-bold">{quantity}</td>
+        <th>
+        <Link to={`/toyDetails/${_id}`}><button className="btn  btn-sm bg-violet-800">View Details</button></Link>  
+        </th>
+        <th>
+          <label>
+          <button className="btn btn-circle btn-sm bg-green-700"><FaArrowCircleUp className="h-6 w-6"/>
+  
+</button>
+          </label>
+        </th>
+        <th>
+          <label>
+          <button onClick={()=>handleDelete(_id) } className="btn btn-circle btn-sm bg-red-800">
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+</button>
+          </label>
+        </th>
+      </tr>
+    );
+};
+
+export default MyToyRows;
--- src/Components/Toys/MyToys.jsx
+++ src/Components/Toys/MyToys.jsx
@@ -1,22 +1,88 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
+import MyToyRows from "./MyToyRows";
+import Swal from 'sweetalert2'
 
 
 const MyToys = () => {
 const {user} = useContext(AuthContext);
-const[toy,setToy] = useState([]);
+const[toys,setToys] = useState([]);
+const handleDelete = (id) =>{
+  console.log(id)
 
+  Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'danger',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+  fetch(`http://localhost:5000/toys/${id}`, {
+  
+      method:"DELETE",
+   
+  })
+  .then(res => res.json())
+  .then (data => {console.log(data)
+  if(data.deletedCount>0){
+      Swal.fire(
+              'Deleted!',
+              'Toy is deleted',
+              'success'
+      )
+      const remainingToys = toys.filter(toy=> toy._id !== id)
+      setToys(remainingToys)
+  }
+  })
+}})
+
+ }
     const uri = `http://localhost:5000/toys?sellerEmail=${user?.email}`
   useEffect(()=>{
     fetch(uri)
     .then (res=> res.json())
     .then (data => {console.log(data)
-    setToy(data)})
+    setToys(data)})
   },[uri])
     return (
-        <div>
-            <h2>My Toys: {toy.length}</h2>
-        </div>
+      <div className="bg-violet-300">
+      <div className="text-center">  <h3 className="text-4xl text-center font-bold text-yellow-700 p-3">My Toys </h3></div>
+        <div className="overflow-x-auto w-full">
+<table className="table w-full">
+{/* head */}
+<thead>
+  <tr>
+    <th>
+      
+    </th>
+    <th>Toy Image & Name</th>
+    <th>Seller Name</th>
+    <th>Sub-category</th>
+  
+    
+    <th>Price</th>
+    <th>A. Quantity</th>
+    <th>Details</th>
+    <th>Update</th>
+    <th>Delete</th>
+   
+  </tr>
+</thead>
+<tbody>
+  {
+    toys.map(toy=> <MyToyRows key={toy._id} toy={toy} handleDelete={handleDelete}></MyToyRows>)
+  }
+
+
+</tbody>
+
+
+</table>
+</div>
+    </div>
     );
 };
 
